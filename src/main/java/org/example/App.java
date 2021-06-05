e58ecad8bc424ac69f7d41fa07a8183bdda91ad1
Anisha Desktop <anisharanjan55@gmail.com>
2021-06-05 02:40:57 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 21 Solution
+ *  Copyright 2021 Anisha Ranjan
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
